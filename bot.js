e886f89e48ccee07027bc9e8c2244d39173ace57
TheCactusMonkey <30912172+TheCactusMonkey@users.noreply.github.com>
2018-10-29 17:14:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(BOT_TOKEN); //add your own Discord bot token
 
+const prefix = "/" //bot command prefix
 var request = require('request');
-var mcCMD = '/ping'; //command to trigger
+var CMD = 'ping'; //command to trigger
 var mcIP = 'mc.hypixel.net'; //add your Minecraft server IP
 var mcPort = 25565; //the port of the server, default it 25565
 
+//server ping message
 client.on('message', message => {
-    if (message.content === mcCMD) {
+    if (message.content === prefix + CMD) {
         var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
         request(url, function(err, response, body) {
             if(err) {
@@ -28,4 +30,12 @@ client.on('message', message => {
             message.channel.send(status);
         });
     }
+});
+
+//bot status
+bot.user.setStatus('Online')
+
+//bot game displays prefix and command
+client.on("ready"), () => {
+    client.user.setGame(prefix + CMD)
 });
