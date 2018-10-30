20d94f1c1b710bff0f3c271f5e28643cae7d4c42
TheCactusMonkey <30912172+TheCactusMonkey@users.noreply.github.com>
2018-10-30 15:44:49 +0100

    Working Embed Server Status

--- bot.js
+++ bot.js
@@ -4,13 +4,16 @@ bot.login('BOT_TOKEN'); //Add your own Discord bot token
 
 const prefix = "/" //Bot command prefix
 var request = require('request');
-var CMD = 'PING'; //Command to trigger, keep it UPPERCASE!
-var mcIP = 'mc.hypixel.net'; //Add your Minecraft server IP
+var CMD = 'ping'; //Command to trigger
+var mcIP = 'mc.server.net'; //Add your Minecraft server IP
 var mcPort = 25565; //The port of the server, default it 25565
 var serverName = 'Minecraft Server'; //Your server name
+var serverUrl = "https://minecraft.net"; //Server website
+var serverLogo = "https://images-eu.ssl-images-amazon.com/images/I/512dVKB22QL.png"; //Server logo
+var color = 16711680 //Hex color for the embed, use 0x instead of #
 
 bot.on('message', message => {
-  let msg = message.content.toUpperCase();
+
   if (message.content === prefix + CMD) {
     var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
     request(url, function (err, response, body) {
@@ -18,19 +21,47 @@ bot.on('message', message => {
         console.log(err);
         return message.reply('Error getting Minecraft server status...');
       }
+
       body = JSON.parse(body);
-      var status = '__Server Status:__ **' + serverName + '** is currently offline';
+      var status = "Offline"
       if (body.online) {
-        status = '__Server Status:__ **' + serverName + '** is **online**  -  ';
-        if (body.players.now) {
-          status += '**' + body.players.now + '/' + body.players.max + '** players are online!';
-        } else {
-          status += '*Noone are online!*';
-        }
+        status = "Online"
       }
-      message.channel.send(status);
+      var players = 0
+      if (body.players.now) {
+        players += body.players.now;
+      }
+      else {
+        status += 0
+      }
+
+      const embed = {
+        "author": {
+          "name": serverName + " Server Status",
+          "url": serverUrl,
+          "icon_url": serverLogo
+        },
+        "color": color,
+        "fields": [
+          {
+            "name": "Status:",
+            "value": status,
+            "inline": true
+          },
+          {
+            "name": "Players Online:",
+            "value": "**" + body.players.now + "** / **" + body.players.max + "**",
+            "inline": true
+          }
+        ],
+        "footer": {
+          "text": "IP: " + mcIP
+        }
+      };
+      message.channel.send({ embed });
+
     });
-  }
+  };
 });
 
 bot.on('ready', () => {
