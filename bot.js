fd106179bcf2a753e040dc812964247dfcb38363
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-04-25 12:35:24 +0200

    Remove/rename old variables

--- bot.js
+++ bot.js
@@ -5,17 +5,10 @@ const fetch = require('node-fetch');
 bot.login('BOT_TOKEN'); //Add your own Discord bot token
 const {prefix, command, token, mcIP, mcPort, serverName, serverUrl, serverLogo} = require('./config.json');
 
-const prefix = "/" //Bot command prefix
-const CMD = 'ping'; //Command to trigger
-let mcIP = 'mc.server.net'; //Add your Minecraft server IP
-let mcPort = 25565; //The port of the server, default it 25565
-let serverName = 'Minecraft Server'; //Your server name
-let serverUrl = "https://minecraft.net"; //Server website
-let serverLogo = "https://images-eu.ssl-images-amazon.com/images/I/512dVKB22QL.png"; //Server logo
 
 bot.on('message', message => {
 
-	if (message.content === prefix + CMD) {
+	if (message.content === prefix + command) {
 		let url = `http://mcapi.us/server/status?ip=${mcIP}&port=${mcPort}`;
 		
 		(async () => {
